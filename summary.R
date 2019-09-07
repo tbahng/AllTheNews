d37de9e86cdb54fd8af6ad548852279cb2bab330
Tim Zalk <tim.zalk@gmail.com>
2019-09-07 16:32:01 -0700

    Update summary.R

--- summary.R
+++ summary.R
@@ -1,1 +1,16 @@
 # Summary statistics and information about the data source
+
+path_transformed <- "data/2Transform.rda"
+path_extracted <- "data/1Extract.rda"
+load(path_transformed)
+load(path_extracted)
+
+# Variables for articles data frame and factors of each variable
+names(df)
+levels(df$year)
+levels(df$month)
+levels(df$publication)
+levels(df$category)
+levels(df$digital)
+
+# Plot of 
